ef005cfaff3e2c61c43413158350f90369598465
Elianabla <26eblankstein@gannacademy.org>
2024-10-07 12:49:51 -0400

    HI

--- src/App.java
+++ src/App.java
@@ -2,16 +2,25 @@ import processing.core.*;
 
 public class App extends PApplet {
     double score = 0;
-    int cookiesPerClick = 1;
+    double cookiesPerClick = 1;
     PImage cookieclick;
     int row = 0;
-    double cookiesGainedPerSec = 0.5;
+    double cookiesGainedPerSec = 0;
     int bgColor = color(210, 210, 210);
-    int costbox1 = 100;
-    int costbox2 = 300;
+    int costbox1 = 75;
+    int costbox2 = 200;
     int costbox3 = 600;
-    int costbox7 = 10000;
+    int costbox4 = 900;
+    int costbox5 = 1200;
+    int costbox9 = 10000;
+    int costbox10 = 20000;
     int scene = 0;
+    int lastCookie = 0;
+    PImage house;
+    PImage cookie2;
+    PImage cookie3;
+    PImage cookie4;
+    PImage currentImage;
 
     public static void main(String[] args) {
         PApplet.main("App");
@@ -23,43 +32,52 @@ public class App extends PApplet {
 
     public void setup() {
         cookieclick = loadImage("cookieclick.png");
+        house = loadImage("house.png");
+        cookie2 = loadImage("cookie 2.png");
+        cookie3 = loadImage("cookie 3.png");
+        cookie4 = loadImage("cookie 4.png");
+        currentImage = cookieclick;
     }
 
     public void draw() {
         if (scene == 0) {
-            background(123,63,0); // sets a background color for the loading page
+            background(123, 63, 0); // sets a background color for the loading page
             textSize(32);
             fill(193, 154, 107);
-            text("Cookie Clicker Game", 260 ,200); // title of game 
+            text("Cookie Clicker Game", 260, 200); // title of game
 
             // draw the "play" button
             fill(193, 154, 107);
             rect(340, 300, 120, 50); // position and size of the button
 
-            fill(123,63,0);
+            fill(123, 63, 0);
             textSize(20);
-            text("Play", 375, 330); // text for the button
+            text("Play", 380, 330); // text for the button
 
             fill(193, 154, 107);
             rect(340, 400, 120, 50);
 
-            fill(123,63,0);
+            fill(123, 63, 0);
             textSize(20);
             text("Instructions", 350, 430);
 
-        } else if (scene  == 2) {
+        } else if (scene == 2) {
             background(123, 63, 0);
+            image(house, 20, 550, 40, 40);
             textSize(32);
-            fill (193, 154 , 107);
-            text("Instructions: ", 260, 200);
+            fill(193, 154, 107);
+            text("Instructions: ", 290, 230);
+            text("Click the cookies and work on \n upgrading them by using the \n upgrades bar on the right.", 200,
+                    300);
 
         } else if (scene == 1) {
             background(bgColor);
-            image(cookieclick, 175, 175, 200, 200);
+            image(currentImage, 175, 175, 200, 200);
+            image(house, 20, 550, 40, 40);
             textSize(20);
             fill(255);
             text("Cookies clicked: " + score, 10, 30);
-            text(cookiesPerClick + " cookie per click", 10, 60);
+            text(cookiesPerClick + " cookies per click", 10, 60);
             text(cookiesGainedPerSec + " cookies gained per second", 10, 90);
 
             stroke(255);
@@ -96,18 +114,26 @@ public class App extends PApplet {
                 scene = 1; // Switch to the game scene
             }
 
-            if (mouseX>= 340 && mouseX <= 460 && mouseY >= 400 && mouseY <= 450) {
+            if (mouseX >= 340 && mouseX <= 460 && mouseY >= 400 && mouseY <= 450) {
                 scene = 2;
             }
-            
+
         } else if (scene == 1) {
+            if (mouseX >= 20 && mouseX <= 60 && mouseY >= 550 && mouseY <= 590) {
+                scene = 0;
+            }
             float distance = dist(mouseX, mouseY, 275, 275);
-    
+
             if (distance <= 100) {
                 score += cookiesPerClick;
+
             }
-    
+
             upgradePressed(mouseX, mouseY);
+        } else if (scene == 2) {
+            if (mouseX >= 20 && mouseX <= 60 && mouseY >= 550 && mouseY <= 590) {
+                scene = 0;
+            }
         }
     }
 
@@ -133,53 +159,118 @@ public class App extends PApplet {
         }
         square(575, 165, 95);
 
-        if (score >= costbox7) {
+        if (score >= costbox4) {
             fill(140, 255, 140);
         } else {
             fill(255);
         }
-        square(575, 375, 95);
+        square(680, 165, 95);
+
+        if (score >= costbox5) {
+            fill(140, 255, 95);
+        } else {
+            fill(255);
+        }
+
+        square(575, 270, 95);
+
+        if (score >= costbox9) {
+            fill(140, 255, 140);
+        } else {
+            fill(255);
+        }
+        square(575, 480, 95);
+
+        if (score >= costbox10) {
+            fill(140, 255, 140);
+        } else {
+            fill(255);
+        }
+
+        square(680, 480, 95);
     }
 
     public void keyPressed() {
         if (key == 'p') {
             score += 1000;
         }
+
     }
 
     public void text() { // method for all of the text I am using in the grid
         textSize(12);
-        text("Cookies increase \n by 3 cookies \n per click \n Cost: " + costbox1, 575, 80);
-        text("Increase cookies \n gained per  \n second by 2  \n Cost: " + costbox2, 680, 80);
-        text("Cookies increase \n by 5 cookies \nper click \nCost: " + costbox3, 575, 185);
-        text("Change \nbackground \n color \nCost: " + costbox7, 575, 395);
+        text("Cookies increase \n by 1 cookie \n per click \n Cost: " + costbox1, 575, 80);
+        text("Increase cookies \n gained per  \n second by 0.5 \n Cost: " + costbox2, 680, 80);
+        text("Cookies increase \n by 3 cookies \nper click \nCost: " + costbox3, 575, 185);
+        text("Increase cookies \n gained per \n second by 1 \n Cost: " + costbox4, 680, 185);
+        text("Cookies increase \n by 5 cookies \n per click \n Cost: " + costbox5, 575, 290);
+
+        text("Change \nbackground \n color \nCost: " + costbox9, 575, 500);
+        text("Change cookie \n kind \n \n Cost: " + costbox10, 680, 500);
+
     }
 
     public void upgradePressed(int x, int y) {
 
         if (score >= costbox1 && x >= 575 && x <= 670 && y >= 60 && y <= 155) {
             fill(230);
-            cookiesPerClick += 3;
+            cookiesPerClick += 1;
             score -= costbox1; // makes the score decrease by the amount the upgrade was bought for
-            costbox1 += 100; // makes the cost go up by 100 each time its purchased
+            costbox1 *= 1.2; // makes the cost go up by 100 each time its purchased
 
         }
         if (score >= costbox2 && x >= 680 && x <= 775 && y >= 60 && y <= 155) {
-            cookiesGainedPerSec += 2;
+            cookiesGainedPerSec += 0.5;
             score -= costbox2;
-            costbox2 += 200;
+            costbox2 *= 1.3;
         }
 
         if (score >= costbox3 && x >= 575 && x <= 670 && y >= 165 && y <= 260) {
-            cookiesPerClick += 5;
+            cookiesPerClick += 3;
             score -= costbox3;
-            costbox3 += 300;
+            costbox3 *= 1.4;
         }
 
-        if (score >= costbox7 && x >= 575 && x <= 670 && y >= 395 && y <= 500) {
+        if (score >= costbox4 && x >= 680 && x <= 775 && y >= 165 && y <= 260) {
+            cookiesGainedPerSec += 1;
+            score -= costbox4;
+            costbox4 *= 1.5;
+        }
+
+        if (score >= costbox5 && x >= 575 && x <= 680 && y >= 270 && y <= 365) {
+            cookiesPerClick += 5;
+            score -= costbox5;
+            costbox5 *= 1.5;
+        }
+
+        if (score >= costbox9 && x >= 575 && x <= 670 && y >= 480 && y <= 575) {
             bgColor = color(random(0, 256), random(0, 256), random(0, 256));
-            score -= costbox7;
-            costbox7 += 600;
+            score -= costbox9;
+            costbox9 += 600;
+        }
+        if (score >= costbox10 && x >= 680 && x <= 775 && y >= 480 && y <= 575) {
+            randomCookie();
+            score -= costbox10;
+            costbox10 += 1000;
+        }
+    }
+
+    public void randomCookie() {
+        int choice = (int) random(4);
+
+        if (choice == lastCookie) {
+            choice = (int) random(4);
+        }
+        lastCookie = choice;
+
+        if (choice == 0) {
+            currentImage = cookieclick;
+        } else if (choice == 1) {
+            currentImage = cookie2;
+        } else if (choice == 2) {
+            currentImage = cookie3;
+        } else if (choice == 3) {
+            currentImage = cookie4;
         }
     }
 }
